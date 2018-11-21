6f9d95cdce0a6a7a8df32c8aa9a11d26c747770c
varlock3 <44277067+varlock3@users.noreply.github.com>
2018-11-21 22:09:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -367,8 +367,8 @@ client.on('message', message => { // Leaked by [ @M3a4x ]
             if(!message.channel.guild) return;
 var args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('-bcall')){
- if (message.author.id !== '456641975932813345') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
- if(!message.author.id === '456641975932813345') return;
+ if (message.author.id !== '464046823162511400') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if(!message.author.id === '464046823162511400') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
